6be442f216e8ee05dfbd72a2c968c0d96669a607
FlavioCir <113441260+FlavioCir@users.noreply.github.com>
2022-10-10 15:19:41 +0200

    Add files via upload

---
+++ Test1/assets/js/index.js
@@ -0,0 +1,4 @@
+alert('Pop up elemento bloccante, premere OK per proseguire');
+window.alert('Primi studi JS');
+
+window.alert(3 + 7)
---
+++ Test1/assets/js/naming.js
@@ -0,0 +1,30 @@
+var pet_preferito = 'gatto'; //scrittura della variabile sbagliata
+
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'criceto';
+setInterval(function() {
+    document.getElementById('gatto').innerHTML = petPreferito; 
+}, 4000 )
+var PetPreferito = 'coniglio';
+setInterval(function() {
+    document.getElementById('cane').innerHTML = PetPreferito;
+}, 4000 )
+
+var $pet = 'giraffa';
+setInterval(function() {
+    document.getElementById('coniglio').innerHTML = $pet;
+}, 4000 )
+console.log($pet);
+var _pet = 'leone';
+setInterval(function() {
+    document.getElementById('farfalla').innerHTML = _pet;
+}, 4000 )
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+
+//var 12 = 'numero';
